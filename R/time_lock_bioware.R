a763126fe5f709448d2e6a72ca79440ab296ed88
Raphael <36821713+RaphaelHartmann@users.noreply.github.com>
2023-12-01 12:31:23 +0000

    Update time_lock_bioware.R

--- R/time_lock_bioware.R
+++ R/time_lock_bioware.R
@@ -110,6 +110,12 @@ time_lock_fp_data <- function(fp.dt, vars,
         }
       }
       tmp.ind <- which(event.info$values %in% time.lock.trigger)
+      if (length(tmp.ind) > 1) {
+        message(paste0("found more than one time-lock trigger in trial ", fp.dt.copy$trial[i], 
+                       " block ", fp.dt.copy$block[i], " subj ", fp.dt.copy$subj[i], 
+                       "\nOnly the first one is used! Please make sure this makes sense\n"))
+        tmp.ind <- tmp.ind[1]
+      }
       lock.info <- list(zero = event.info$onset[tmp.ind])
       lock.info$lower <- sapply(bins.dp, function(x) lock.info$zero + x[1])
       lock.info$upper <- sapply(bins.dp, function(x) lock.info$zero + x[2])
